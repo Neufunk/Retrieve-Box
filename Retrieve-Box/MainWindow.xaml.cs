a2a50e45d02a9850d3a9ce3d4bd0a14e531d9fdc
Neufunk <mail.johnathan@gmail.com>
2018-08-10 15:38:41 +0200

    E-mail function added

--- Retrieve-Box/MainWindow.xaml.cs
+++ Retrieve-Box/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ namespace RetrieveBox {
 
         private DataSet ds = new DataSet();
         private DataTable dt = new DataTable();
-        string sql = "SELECT name, phone FROM phone_number";
+        string sql = "SELECT name, phone, mail FROM phone_number";
 
         public MainWindow() {
             InitializeComponent();
@@ -25,15 +25,15 @@ namespace RetrieveBox {
 
             if (String.IsNullOrEmpty(textBox.Text)) {
                 ds.Reset();
-                sql = "SELECT name, phone FROM phone_number";
+                sql = "SELECT name, phone, mail FROM phone_number";
                 StartQuery();
             } else {
-                sql = "SELECT name, phone FROM phone_number WHERE name ILIKE '%" + userInput + "%' OR phone ILIKE '%" + userInput +
+                sql = "SELECT name, phone, mail FROM phone_number WHERE name ILIKE '%" + userInput + "%' OR phone ILIKE '%" + userInput +
                     "%' OR centre ILIKE '%" + userInput + "%'";
                 StartQuery();
             }
         }
-
+        
         private void StartQuery() {
             try {
                 // PostgeSQL-style connection string
@@ -61,6 +61,31 @@ namespace RetrieveBox {
 
         private void Textbox_Loaded(object sender, RoutedEventArgs e) {
             textBox.Focus();
+        }
+
+        private new void MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            DataRowView selectedRow = gridView.SelectedItem as DataRowView;
+            string email = selectedRow["mail"].ToString();
+            Console.WriteLine("DoubleClick triggered on e-mail :" + email);
+
+            Microsoft.Office.Interop.Outlook.Application oApp = new Microsoft.Office.Interop.Outlook.Application();
+            Microsoft.Office.Interop.Outlook.MailItem oMsg = (Microsoft.Office.Interop.Outlook.MailItem)oApp.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem);
+
+            oMsg.Recipients.Add(email);
+            oMsg.BodyFormat = Microsoft.Office.Interop.Outlook.OlBodyFormat.olFormatHTML;
+            oMsg.HTMLBody = "";
+            oMsg.Display(false); //In order to display it in modal inspector change the argument to true
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show("     INFO\n" +
+                "     ---------\n" +
+                "   Vous pouvez rechercher par : \n" +
+                "   - NOM / PRÉNOM \n" +
+                "   - NUMÉRO \n" +
+                "   - CENTRE \n" +
+                "\n" +
+                "   Double-cliquez sur un nom pour ouvrir un nouveau mail à destination de cette personne.");
         }
     }
 }
